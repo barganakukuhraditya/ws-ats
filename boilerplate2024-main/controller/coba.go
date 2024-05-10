19513c2211af77762f0d560b8b6fc52a474d840d
barganakukuhraditya <barganakukuh@gmail.com>
2024-05-10 11:01:18 +0700

    Test Package be_ats

--- boilerplate2024-main/controller/coba.go
+++ boilerplate2024-main/controller/coba.go
@@ -3,9 +3,19 @@ package controller
 import (
 	"github.com/aiteung/musik"
 	"github.com/gofiber/fiber/v2"
+	cek "github.com/barganakukuhraditya/be_ats/module"
 )
 
 func Homepage(c *fiber.Ctx) error {
 	ipaddr := musik.GetIPaddress()
 	return c.JSON(ipaddr)
+}
+
+func GetAllJadwal(c *fiber.Ctx) error {
+    ps, err := cek.GetAllJadwal()
+    if err != nil {
+        return err
+    }
+    
+    return c.JSON(ps)
 }
